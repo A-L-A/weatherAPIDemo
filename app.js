b5f653e7704caf9d3616ea801978b09c6a1568ab
A-L-A <64477195+A-L-A@users.noreply.github.com>
2023-06-07 11:16:15 +0200

    weather api update 2

--- app.js
+++ app.js
@@ -1,31 +1,52 @@
-const { log } = require("console");
-const express =require("express");
+
+const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.get("/", function(req, res){
-    
-const url ="https://api.openweathermap.org/data/2.5/weather?q=Kigali&appid=9b892efc02d6461e7d846186ea05fdea&units=metric";
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
-    https.get(url, function(response){
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const query = req.body.cityName;
+  const apiKey = "9b892efc02d6461e7d846186ea05fdea";
+  const unit = "metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    unit;
+
+  https.get(url, function (response) {
     console.log(response.statusCode);
-    
-    response.on("data", function(data){
-        
-       const weatherData = JSON.parse(data)
-       const temp = weatherData.main.temp
-       const weatherDescription = weatherData.weather[0].description
-       const icon = weatherData.weather[0].icon;
-       const image = "https://openweathermap.org/img/wn/" +icon+ "@2x.png";
-       console.log(weatherDescription)
-       res.write("<p>The weather is currently " + weatherDescription + " </p>");
-       res.write("<h1>The temperature in Kigali is " + temp + " degrees celcius</h1>");
-       res.write("<img src = "+ image+ ">")
-       res.send();
-    })
-    })
-})
 
-app.listen(3000, function(){
-    console.log("Server is running on port 3000");
-})
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+     const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const image = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      console.log(weatherDescription);
+      res.write("<p>The weather is currently " + weatherDescription + " </p>");
+      res.write(
+        "<h1>The temperature in " +
+          query +
+          " is " +
+          temp +
+          " degrees celcius</h1>"
+      );
+      res.write("<img src = " + image + ">");
+      res.send();
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
